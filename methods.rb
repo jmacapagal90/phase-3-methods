27e1dfd5c4effce00cc02617b301c5497b7bbe52
James Macapagal <j.macapagal@gmail.com>
2022-06-27 11:21:21 -0500

    commit tree 0309eb49173eb051709aedc0af881bdcfd113862

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!" 
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+    puts num1 + num2
+    num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    puts num/ 2
+    num / 2
+end
+
+add(1,1)
+halve(1)
